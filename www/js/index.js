03660f688056e98fec91fc1613fa9ff6eb2b1bef
jcridge <Wanderer8594>
2014-10-10 11:06:10 +0100

    h

--- www/js/index.js
+++ www/js/index.js
@@ -43,3 +43,19 @@ var launched_count = 0;
 		test();
 		updateDisplay();
     }
+	
+	window.plugins.flashlight.available(function(isAvailable) {
+	if (isAvailable) {
+
+    // switch on
+    window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+
+    // switch off after 3 seconds
+    setTimeout(function() {
+      window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+    }, 3000);
+
+	} else {
+		alert("Flashlight not available on this device");
+	}
+	});
